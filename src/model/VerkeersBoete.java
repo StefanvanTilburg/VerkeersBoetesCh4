f01850609a6009fe503124625bb7f4781022863f
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-09-26 17:08:54 +0200

    Verkeersboetes Stap 1 resultaat

--- src/model/VerkeersBoete.java
+++ src/model/VerkeersBoete.java
@@ -8,4 +8,39 @@ package model;
  * Doel     Class VerkeersBoete. Registratie : naamOvertreder, kenteken, bedrag
  */
 public class VerkeersBoete {
+    private String naamDader;
+    private String kentekenAuto;
+    private double bedrag;
+
+    public VerkeersBoete() {
+        this("Onbekend", "XX-XX-XX");
+    }
+
+    public VerkeersBoete(String naamDader, String kentekenAuto) {
+        this(naamDader, kentekenAuto, 0.0);
+    }
+
+    private VerkeersBoete(String naamDader, String kentekenAuto, double bedrag) {
+        this.naamDader = naamDader;
+        this.kentekenAuto = kentekenAuto;
+        this.bedrag = bedrag;
+    }
+
+    public double bepaalBedrag() {
+        return 0.0;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Verkeersboete voor %s, kenteken %s, %.2f euro",
+                            this.naamDader, this.kentekenAuto, this.getBedrag());
+    }
+
+    public double getBedrag() {
+        return bedrag;
+    }
+
+    public void setBedrag(double bedrag) {
+        this.bedrag = bedrag;
+    }
 }
