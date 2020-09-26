4d766e455aad57b602cdc435427cc4d30cc5fcd7
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-09-26 16:59:01 +0200

    init repo Verkeersboetes

---
+++ src/controller/VerkeersBoetesLauncher.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * @author Stefan van Tilburg
+ * <p>
+ * Opdracht 4.2 Verkeersboetes
+ * <p>
+ * Doel     OOP applicatie om boetes te beoordelen, waarbij hoogte afhankelijk is van type overtreding.
+ */
+public class VerkeersBoetesLauncher {
+}
---
+++ src/model/VerkeersBoete.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * @author Stefan van Tilburg
+ * <p>
+ * Opdracht 4.2 Verkeersboetes
+ * <p>
+ * Doel     Class VerkeersBoete. Registratie : naamOvertreder, kenteken, bedrag
+ */
+public class VerkeersBoete {
+}
