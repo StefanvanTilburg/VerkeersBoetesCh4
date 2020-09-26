ae3d76cd02afd7619d3b0ded2ca72a1eb3ff721e
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-09-26 17:54:20 +0200

    Verkeersboetes Stap 4 resultaat

--- src/controller/VerkeersBoetesLauncher.java
+++ src/controller/VerkeersBoetesLauncher.java
@@ -1,5 +1,9 @@
 package controller;
 
+import model.ParkeerBoete;
+import model.SnelheidsBoete;
+import model.VerkeersBoete;
+
 /**
  * @author Stefan van Tilburg
  * <p>
@@ -8,4 +12,18 @@ package controller;
  * Doel     OOP applicatie om boetes te beoordelen, waarbij hoogte afhankelijk is van type overtreding.
  */
 public class VerkeersBoetesLauncher {
+    public static void main(String[] args) {
+        VerkeersBoete verkeersBoete = new VerkeersBoete("Hallo","EersteDader");
+        System.out.println(verkeersBoete.toString());
+        System.out.println();
+
+        ParkeerBoete parkeerBoete = new ParkeerBoete("Test", "xx-xx-MINE",2);
+        System.out.println(parkeerBoete.toString());
+        System.out.println();
+
+        SnelheidsBoete snelheidsBoete = new SnelheidsBoete("Test 2", "MICROP",
+                120, 100);
+        System.out.println(snelheidsBoete.toString());
+        System.out.println();
+    }
 }
